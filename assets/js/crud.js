b1bcf7bddbfecae5c72cdfe6ddf0cb192ddb38d0
EDAM19 <eabregomendoza@hotmail.com>
2022-04-10 17:57:05 -0500

    add crud functions

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -17,3 +17,71 @@ $(function() {
         firstDay: 0
     });
 });
+
+const inputFecha = document.getElementById('datepicker')
+const inputConsola = document.getElementById('consola')
+const inputFalla = document.getElementById('falla')
+
+const btnAgregar = document.getElementById('agregar')
+const btnEditar = document.getElementById('editar')
+
+let arr = [];
+let pos = 0;
+
+btnAgregar.addEventListener('click', () => {
+    let cita = {
+        fecha: inputFecha.value,
+        consola: inputConsola.value,
+        falla: inputFalla.value
+    }
+    arr.push(cita)
+    inputFecha.value = ''
+    inputConsola = ''
+    inputFalla = ''
+    localStorage.setItem('citas', JSON.stringify(arr))
+    renderElemento()
+})
+
+function renderElemento() {
+    const lista = document.getElementById('lista-citas')
+    lista.innerHTML = ''
+    arr.forEach(function(item, i) {
+        lista.innerHTML += `
+        <li>
+        <div class="datos_cita">
+          <p>Fecha: ${item.fecha}</p>
+          <p>Consola: ${item.consola}</p>
+          <p>Falla: ${item.falla}</p>
+        </div>
+        <div class="btn_cita">
+          <button class="danger" onclick="borrarElemento(${i})" >Eliminar</button>
+          <button class="primary" onclick="editar(${i})" >Editar</button>
+        <div>
+        </li>
+      `
+    })
+}
+
+function borrarElemento(posicion) {
+    arr.splice(posicion, 1)
+    localStorage.setItem('citas', JSON.stringify(arr))
+    if (arr.length == 0) {
+        localStorage.clear()
+    }
+    renderElemento()
+}
+
+function getItems() {
+    const citas = localStorage.getItem('citas')
+
+    if (citas != null) {
+        arr = JSON.parse(citas)
+        renderElemento()
+    }
+}
+
+getItems()
+
+Array.prototype.insert = function(index, item) {
+    this.splice(index, 0, item);
+};
